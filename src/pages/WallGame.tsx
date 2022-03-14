6e749312ad008345e5589f301beb2a21670c8bdc
Jenna <jr13114@my.bristol.ac.uk>
2022-03-14 14:39:04 +0100

    add description to wall game project

--- src/pages/WallGame.tsx
+++ src/pages/WallGame.tsx
@@ -5,13 +5,13 @@ export default function WallGame(): JSX.Element {
     <>
       <Navbar />
       <div style={{ textAlign: "center" }}>
-        <h2>Connecting Wall Game Project</h2>
+        <h2>Wall Game Project</h2>
         <a
           href="https://connecting-wall-game.netlify.app/"
           target="_blank"
           rel="noreferrer"
         >
-          <img src="wallgame.png" alt="..." />
+          <img src="wallgame.png" alt="..." width="600px" height="400px" />
         </a>
         <div style={{ fontSize: "19px" }}>
           <a
@@ -23,9 +23,43 @@ export default function WallGame(): JSX.Element {
           </a>
         </div>
       </div>
+      <br />
+      <div
+        style={{
+          paddingLeft: "60px",
+          paddingRight: "60px",
+          fontSize: "14px",
+          textAlign: "justify",
+        }}
+      >
+        <div>
+          Full stack app with a multi-page frontend built using React,
+          JavaScript, TypeScript, the UI was built using HTML/CSS and Bootstrap,
+          and in the backend I built a web server using Express and Node.js, and
+          stored the data in a relational database using PostgreSQL. I wrote
+          some unit tests using Jest for my helper functions in the frontend and
+          plan to write Cypress tests to test various workflows within the app.
+        </div>
+        <br />
+        <div>
+          Users can navigate their way around the homepage which contains the
+          rules and 10 most recent walls. By clicking on a wall name they can
+          play the game by trying to solve the wall. There is also an option to
+          create a new wall, link in the header.
+        </div>
+        <br />
+        <div>
+          The UI has so far been created just for the web as opposed to mobile,
+          but this is something I want to add in. Additional features I will add
+          include having a timer and a rating system, where users will be able
+          to filter based on rating.
+        </div>
+      </div>
       <div style={{ padding: "30px", fontSize: "12px" }}>
         <table className="table">
-          <h4 style={{ paddingLeft: "10px", fontSize: "18px" }}>Other links</h4>
+          <h4 style={{ paddingLeft: "10px", fontSize: "18px" }}>
+            <b>Other links</b>
+          </h4>
           <thead></thead>
           <tbody>
             <tr>
